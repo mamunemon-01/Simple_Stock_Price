57b5555b3a6eb4ff11865cf4ea8f4daa44376ac0
Mamun Al Imran <sciemontist@gmail.com>
2024-01-24 23:17:06 +0600

    Added "Last 1 Year (365 days)" functionality for stock data

--- simple_stock_price.py
+++ simple_stock_price.py
@@ -1,11 +1,12 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
+from datetime import date, timedelta
 
 # following method shows texts using markdown
 st.write("""
 	# Stock Price of Google
-	This web app shows prices and volume of sales of Google stock.
+	This web app shows prices and volume of sales of Google stock in the last 1 year (365 days) period.
 """)
 
 # a variable from yfinance
@@ -13,8 +14,13 @@ tickerSymbol='GOOGL'
 
 tickerData = yf.Ticker(tickerSymbol)
 
+# today's date
+current_date = date.today()
+
+one_year_ago_date = current_date - timedelta(days = 365)
+
 # a dataframe
-tickerDF = tickerData.history(period='id', start='2022-04-17', end='2023-04-17')
+tickerDF = tickerData.history(period='id', start = one_year_ago_date, end = current_date)#start='2022-04-17', end='2023-04-17')
 
 st.line_chart(tickerDF.Close)# shows closing prices
 st.line_chart(tickerDF.Volume)# shows volume of sales
